a90286b2bad273fdf33c0dd987b253f900eb022d
tufei <tufei8438@gmail.com>
2016-10-10 21:24:14 +0800

    增加数据模型定义

--- mercury/backend/models.py
+++ mercury/backend/models.py
@@ -13,6 +13,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+from sqlalchemy.orm.instrumentation import ClassManager
 from mercury.backend import db
 
 
@@ -67,11 +68,163 @@ class BaseModel(db.Model):
                     setattr(self, attr, v)
 
 
-class Case(BaseModel):
+class Attachment(BaseModel):
+
+    __tablename__ = 'attachment'
+
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(32), nullable=False)
-    source = db.Column(db.Integer, nullable=False)
-    taskid = db.Column(db.String(64), nullable=False)
-    name = db.Column(db.String(128), nullable=False)
-    type = db.Column(db.Integer, nullable=False)
+    attach_type = db.Column(db.Integer)
+    attach_id = db.Column(db.Integer)
+    media_type = db.Column(db.Integer)
+    media_path = db.Column(db.String(256))
+    create_time = db.Column(db.DateTime, nullable=False, server_default=db.text("CURRENT_TIMESTAMP"))
+
+
+class Case(BaseModel):
+
+    __tablename__ = 'case'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(32), nullable=False, unique=True, server_default=db.text("''"))
+    source = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+    name = db.Column(db.String(128), nullable=False, server_default=db.text("''"))
+    category_type = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+    parent_category_code = db.Column(db.String(16), nullable=False, server_default=db.text("''"))
+    parent_category_name = db.Column(db.String(64), nullable=False, server_default=db.text("''"))
+    category_code = db.Column(db.String(16), nullable=False, server_default=db.text("''"))
+    category_name = db.Column(db.String(64), nullable=False, server_default=db.text("''"))
+    description = db.Column(db.String(512))
+    address = db.Column(db.String(512))
+    longitude = db.Column(db.String(64))
+    latitude = db.Column(db.String(64))
+    grid_code = db.Column(db.String(64))
+    obj_code = db.Column(db.String(64))
+    proc_inst_id = db.Column(db.String(64))
+    begin_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    expect_end_time = db.Column(db.DateTime)
+    status = db.Column(db.Integer)
+    priority = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+
+
+class CaseActivity(BaseModel):
+
+    __tablename__ = 'case_activity'
+
+    id = db.Column(db.Integer, primary_key=True)
+    case_id = db.Column(db.Integer, nullable=False, index=True)
+    task_id = db.Column(db.String(64), nullable=False, server_default=db.text("''"))
+    name = db.Column(db.String(64))
+    usercode = db.Column(db.String(32))
+    status = db.Column(db.Integer)
+    description = db.Column(db.String(512))
+    begin_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    expect_end_time = db.Column(db.DateTime)
+    remark = db.Column(db.String(128))
+
+
+class Category(BaseModel):
+
+    __tablename__ = 'category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    parent_id = db.Column(db.Integer, nullable=False, server_default=db.text("'0'"))
+    type = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+    code = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    longcode = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    name = db.Column(db.String(64), nullable=False, server_default=db.text("''"))
+    description = db.Column(db.String(256))
+    icon = db.Column(db.String(256))
+    order = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+
+
+class Component(BaseModel):
+
+    __tablename__ = 'component'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    name = db.Column(db.String(64), nullable=False, server_default=db.text("''"))
+    longitude = db.Column(db.String(64))
+    latitude = db.Column(db.String(64))
+    address = db.Column(db.String(256))
+    grid_code = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    description = db.Column(db.String(128))
+    status = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+    collect_time = db.Column(db.DateTime)
+    collect_user = db.Column(db.String(32))
+    collect_dept_id = db.Column(db.Integer)
+    verify_time = db.Column(db.DateTime)
+    verify_user = db.Column(db.String(32))
+    verify_dept_id = db.Column(db.Integer)
+    manage_dept_id = db.Column(db.Integer)
+    owner_dept_id = db.Column(db.Integer)
+    maintain_dept_id = db.Column(db.Integer)
+    update_time = db.Column(db.DateTime, nullable=False)
+
+
+class Department(BaseModel):
+
+    __tablename__ = 'department'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(32), nullable=False, unique=True, server_default=db.text("''"))
+    type = db.Column(db.Integer)
+    name = db.Column(db.String(64))
+    description = db.Column(db.String(512))
+    status = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+    create_time = db.Column(db.DateTime, nullable=False, server_default=db.text("CURRENT_TIMESTAMP"))
+
+
+class Event(BaseModel):
+
+    __tablename__ = 'event'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(32), nullable=False, unique=True, server_default=db.text("''"))
+    name = db.Column(db.String(64), nullable=False, server_default=db.text("''"))
+    region_id = db.Column(db.Integer, nullable=False)
+    dept_id = db.Column(db.Integer)
+    update_time = db.Column(db.DateTime, nullable=False, server_default=db.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+
+
+class Grid(BaseModel):
+
+    __tablename__ = 'grid'
+
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(32), nullable=False, unique=True, server_default=db.text("''"))
+    manage_usercode = db.Column(db.String(32), server_default=db.text("''"))
+    supervisor_usercode = db.Column(db.String(32))
+    status = db.Column(db.Integer)
+    update_time = db.Column(db.DateTime, nullable=False, server_default=db.text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+
+
+class Region(BaseModel):
+
+    __tablename__ = 'region'
+
+    id = db.Column(db.Integer, primary_key=True)
+    parent_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    level = db.Column(db.Integer, nullable=False)
+    code = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    longcode = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    order = db.Column(db.Integer, nullable=False)
+
+
+class User(BaseModel):
+
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    usercode = db.Column(db.String(32), nullable=False, unique=True, server_default=db.text("''"))
+    user_type = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+    name = db.Column(db.String(32), nullable=False, server_default=db.text("''"))
+    phone = db.Column(db.String(16))
+    department_id = db.Column(db.Integer)
+    status = db.Column(db.Integer, nullable=False, server_default=db.text("'1'"))
+    update_time = db.Column(db.DateTime)
+    create_time = db.Column(db.DateTime, nullable=False, server_default=db.text("'0000-00-00 00:00:00'"))
 
